2a178d68c7ec52c3857c8fc9ef0df68b75064c90
jhson929 <sjh05109@gmail.com>
2022-08-17 20:54:22 +0900

    [FEAT] save study type test

--- src/main/java/com/example/eduwithbe/UserTest/Repository/UserTestRepository.java
+++ src/main/java/com/example/eduwithbe/UserTest/Repository/UserTestRepository.java
@@ -2,6 +2,8 @@ package com.example.eduwithbe.UserTest.Repository;
 
 import com.example.eduwithbe.UserTest.Domain.UserTestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserTestRepository extends JpaRepository<UserTestEntity, String> {
 }
